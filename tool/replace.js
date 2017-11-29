8b1397782d0b43df0a6e8a6a7685c3733d16e3ba
MarsCrypto <mars@marscrypto.com>
2017-11-29 22:32:04 +0800

    update

--- tool/replace.js
+++ tool/replace.js
@@ -108,10 +108,14 @@ const options5 = {
     from: [
               /Lisk/g,
               /lisk/g,
+              /d121d3abf5425fdc0f161d9ddb32f89b7750b4bdb0bff7d18b191d4b4bafa6d4/g,
+              /c96dec3595ff6041c3bd28b76b8cf75dce8225173d1bd00241624ee89b50f2a8/g,
           ],
     to:   [
               'Onz',
-              'onz'
+              'onz',
+              '9ee632b8cc594dc969362dcfbddc458083c4d3ef66de1bf050947a91fe5eff47',
+              '62b5193a30bd0e4f78fe77da1bd6ad000a555f1abcfd285ff840a1080fec15ef',
           ],
   };
   
