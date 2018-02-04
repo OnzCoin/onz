27c2bdd86e398652e248b22c7af56f77c0514307
MarsCrypto <mars@marscrypto.com>
2018-02-04 16:07:25 +0800

    update genesisblock

--- helpers/constants.js
+++ helpers/constants.js
@@ -74,9 +74,9 @@ module.exports = {
 	minBroadhashConsensus: 51,
 	nethashes: [
 		// Mainnet
-		'570f81c67882dbcbebf8b84205a78a42cd98561683f878dae8e8c7ea2f54033b',
+		'aa14b4d84260e00b6fc033c022a25965629ab0e8a4aafc77e64cad4cf0dc2e00',
 		// Testnet
-		'570f81c67882dbcbebf8b84205a78a42cd98561683f878dae8e8c7ea2f54033b'
+		'aa14b4d84260e00b6fc033c022a25965629ab0e8a4aafc77e64cad4cf0dc2e00'
 	],
 	numberLength: 100000000,
 	requestLength: 104,
--- onz_manager.bash
+++ onz_manager.bash
@@ -13,7 +13,7 @@ logfile=$root_path/logs/onzcoin.log
 set_branch() {
   if [ "$(grep "xxxxxxxxxxxxxxxxxxxxUSE_MASTER_FOR_DEVELOPMENTxxxxxxxxxxxxxxxxxxxxxxxx" $ONZ_CONFIG )" ];then
     GIT_BRANCH="master"
-  elif [ "$(grep "570f81c67882dbcbebf8b84205a78a42cd98561683f878dae8e8c7ea2f54033b" $ONZ_CONFIG )" ];then
+  elif [ "$(grep "aa14b4d84260e00b6fc033c022a25965629ab0e8a4aafc77e64cad4cf0dc2e00" $ONZ_CONFIG )" ];then
     GIT_BRANCH="testnet"
   else
     GIT_BRANCH="unknown"
